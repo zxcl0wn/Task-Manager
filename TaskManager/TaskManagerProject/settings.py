62e1334076f62e5b27f05e3b1e5e3ef232091380
Gleb <m_marple1@mail.ru>
2025-05-28 20:58:09 +1000

    chore: fix settings

--- TaskManager/TaskManagerProject/settings.py
+++ TaskManager/TaskManagerProject/settings.py
@@ -1,3 +1,4 @@
+import os.path
 from pathlib import Path
 from decouple import config
 
@@ -94,6 +95,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
