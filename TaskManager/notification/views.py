5d39aa15f166b90cfd2a58c94605392b3d2ae94c
Gleb <m_marple1@mail.ru>
2025-05-29 19:35:13 +1000

    chore: add notifications list in templates

--- TaskManager/notification/views.py
+++ TaskManager/notification/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import Notification
 
 
 def notifications_list(request):
+    notifications = Notification.objects.all()
     context = {
-
+        'notifications': notifications,
     }
 
     return render(request, 'notification/notifications_list.html', context=context)
