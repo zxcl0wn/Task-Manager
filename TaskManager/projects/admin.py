f365f14a945c202fd0450aea03befb4254eb05e1
Gleb <m_marple1@mail.ru>
2025-05-29 19:32:41 +1000

    feat: add ability to choose user in project model in admin panel

--- TaskManager/projects/admin.py
+++ TaskManager/projects/admin.py
@@ -6,6 +6,13 @@ class ProjectMemberInline(admin.TabularInline):
     model = ProjectMember
     extra = 1
     raw_id_fields = ('user_id',)
+    fields = ('user_id', 'get_username', 'user_role')
+    readonly_fields = ('get_username',)
+
+    def get_username(self, obj):
+        return obj.user_id.username if obj.user_id else '-'
+
+    get_username.short_description = 'Username'
 
 
 @admin.register(Project)
@@ -14,4 +21,14 @@ class ProjectAdmin(admin.ModelAdmin):
     inlines = (ProjectMemberInline,)
 
 
-admin.site.register(ProjectMember)
+@admin.register(ProjectMember)
+class ProjectMemberAdmin(admin.ModelAdmin):
+    list_display = ('project_id', 'user_id', 'get_username', 'user_role')
+    list_select_related = ('user_id',)
+    list_filter = ('user_role',)
+
+    def get_username(self, obj):
+        return obj.user_id.username
+
+    get_username.short_description = 'Username'
+    get_username.admin_order_field = 'user_id__username'
