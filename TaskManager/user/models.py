22a2b3a9db594e887f4d723613942f2efd09e4e8
Gleb <m_marple1@mail.ru>
2025-05-29 18:59:47 +1000

    chore: delete unused profile model

--- TaskManager/user/models.py
+++ TaskManager/user/models.py
@@ -2,10 +2,10 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=False)
-    name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50, blank=False)
-
-    def __str__(self):
-        return self.name
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=False)
+#     name = models.CharField(max_length=50)
+#     email = models.EmailField(max_length=50, blank=False)
+#
+#     def __str__(self):
+#         return self.name
