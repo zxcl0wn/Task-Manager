3f42f9280b4c6eb49fba5d150ffe82b8d9cc69e1
Gleb <m_marple1@mail.ru>
2025-05-29 18:57:18 +1000

    fix: change projects model

--- TaskManager/projects/models.py
+++ TaskManager/projects/models.py
@@ -3,10 +3,22 @@ from user.models import User
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=40)
-    description = models.TextField(blank=True)
-    comment = models.TextField(blank=True)
-    users = models.ManyToManyField(User, blank=False)
+    title = models.CharField(max_length=40, null=True)
+    description = models.TextField(blank=True, null=True)
+    comment = models.TextField(blank=True, null=True)
+    users = models.ManyToManyField(User, through='ProjectMember', blank=False)
 
     def __str__(self):
         return self.title
+
+
+class ProjectMember(models.Model):
+    USER_ROLE_CHOICES = [
+        ('VIEWER', 'viewer'),
+        ('EDITOR', 'editor'),
+        ('OWNER', 'owner')
+    ]
+
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_role = models.CharField(max_length=10, choices=USER_ROLE_CHOICES)
