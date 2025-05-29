5cebfc2aaa061c1da0b73aae213c244affd6b2cf
Gleb <m_marple1@mail.ru>
2025-05-29 18:59:09 +1000

    feat: add display models in admin panel

--- TaskManager/projects/admin.py
+++ TaskManager/projects/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, ProjectMember
 
 
-admin.site.register(Project)
+class ProjectMemberInline(admin.TabularInline):
+    model = ProjectMember
+    extra = 1
+    raw_id_fields = ('user_id',)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+    inlines = (ProjectMemberInline,)
+
+
+admin.site.register(ProjectMember)
