fb741638149f9c0f9c95377a32ad30af3fc0cd59
Gleb <m_marple1@mail.ru>
2025-06-05 16:07:36 +1000

    feat: add slug field to projects app

--- TaskManager/projects/models.py
+++ TaskManager/projects/models.py
@@ -1,15 +1,24 @@
 from django.db import models
+from django.utils.text import slugify
+from unidecode import unidecode
+from django.template import defaultfilters
 from user.models import User
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=40, null=True)
+    title = models.CharField(max_length=40, null=True, unique=True)
+    slug = models.SlugField(max_length=40, unique=True, null=True)
     description = models.TextField(blank=True, null=True)
     comment = models.TextField(blank=True, null=True)
     users = models.ManyToManyField(User, through='ProjectMember', blank=False)
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        value = self.title
+        self.slug = defaultfilters.slugify(unidecode(value))
+        super().save(*args, **kwargs)
 
 
 class ProjectMember(models.Model):
