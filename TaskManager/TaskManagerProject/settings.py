347f1641a9a65e47e87bb7864a6f2bcdf68bf5e6
Gleb <m_marple1@mail.ru>
2025-06-11 20:05:21 +1000

    feat: add getting user from any place

--- TaskManager/TaskManagerProject/settings.py
+++ TaskManager/TaskManagerProject/settings.py
@@ -38,6 +38,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'user.middleware.CurrentUserMiddleware',
 ]
 
 ROOT_URLCONF = 'TaskManagerProject.urls'
